2fffc9168ec3bba693ab2f687969e182bbb3ea23
Dominik Heeb <d1heeb@hsr.ch>
2015-07-19 14:19:45 +0200

    InitialCommitVersion2

---
+++ PersonalCVPageDheeb/App_Start/BundleConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace PersonalCVPageDheeb
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/js/jquery-1.11.2.min.js",
+                        "~/Scripts/js/jquery.inview.min.js",
+                        "~/Scripts/js/jquery.knob.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                        "~/Scripts/js/bootstrap.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/javascripts").Include(
+                        "~/Scripts/js/imagesloaded.pkgd.min.js","~/Scripts/js/isotope.pkgd.min.js","~/Scripts/js/owl.carousel.min.js","~/Scripts/js/scripts.js","~/Scripts/js/smoothscroll.js"));
+        }
+    }
+}
